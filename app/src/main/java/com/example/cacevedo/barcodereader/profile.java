81134adc20d8cef342cc48a5cdc34b718805f790
cacevedo <cacev000@gmail.com>
2018-01-30 15:31:04 -0500

    created profile and medication activity. Also added menu that lets you go through any activity (Home, Profile or Medication)

--- app/src/main/java/com/example/cacevedo/barcodereader/profile.java
+++ app/src/main/java/com/example/cacevedo/barcodereader/profile.java
@@ -3,25 +3,16 @@ package com.example.cacevedo.barcodereader;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
-import javax.xml.transform.Result;
-
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ZXingScannerView scannerView;
+public class profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_profile);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,31 +38,5 @@ public class MainActivity extends AppCompatActivity {
         }
         //respond to menu item selection
 
-    }
-
-    public void scanCode(View view){
-        scannerView = new ZXingScannerView(this);
-        scannerView.setResultHandler(new ZXingScannerResultHandler());
-
-        setContentView(scannerView);
-        scannerView.startCamera();
-    }
-
-//    no need for this piece of code at the moment
-//    @Override
-//    public void onPause(){
-//        super.onPause();
-//        scannerView.stopCamera();
-//    }
-
-    class ZXingScannerResultHandler implements ZXingScannerView.ResultHandler{
-        @Override
-        public void handleResult(com.google.zxing.Result result) {
-            String resultCode = result.getText();
-            Toast.makeText(MainActivity.this, resultCode, Toast.LENGTH_SHORT).show();
-
-            setContentView(R.layout.activity_main);
-            scannerView.stopCamera();
-        }
     }
 }
